f5695a206b03f98ea196661827debb764a53fcbd
Gabbee24 <atunwagabriel@gmail.com>
2022-10-09 16:44:03 +0100

    added the addtoCart feature

--- my-app/src/E-commerce/components/cart-item/cart-item.component.jsx
+++ my-app/src/E-commerce/components/cart-item/cart-item.component.jsx
@@ -4,9 +4,11 @@ export default function CartItem({cartItem}) {
     const{name, imageURRL, price, quantity} = cartItem;
     return(
         <div className='cart-item-container'>
-            <img src={imageURRL} alt={``} />
-            <h2>{name}</h2>
-            <span>{quantity}</span>
+            <img src={imageURRL} alt={`${name}`} />
+            <div className="item-details">
+                <span className='name'>{name}</span>
+                <span className='price'>{quantity} x ${price}</span>
+            </div>
         </div>
     );
 };
